16b920cb3eb6d79caef24324d05593f469b1cba1
Bayu Tantra Bramandhita <73697375+bayutb123@users.noreply.github.com>
2023-09-03 00:39:52 +0700

    Fix Image Bug at Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,6 +10,9 @@ class DashboardController extends Controller
     public function index()
     {
         $latestblogs = Blog::latest()->take(3)->orderBy('id', 'DESC')->get();
+        foreach ($latestblogs as $blog) {
+            $blog->content = str_replace("uploads/", "/uploads/", $blog->content); // Fix image path
+        }
         $user = auth()->user();
         return view('dashboard.index')->with('user', $user)->with('latestblogs', $latestblogs);
     }
