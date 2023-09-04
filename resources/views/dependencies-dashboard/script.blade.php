5c07de921e4d4f1b44ffd9a943fae5e0725ed9b4
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-09-04 08:01:38 +0700

    Footer and Locale

--- resources/views/dependencies-dashboard/script.blade.php
+++ resources/views/dependencies-dashboard/script.blade.php
@@ -62,26 +62,6 @@
             formData.append('file', blobInfo.blob(), blobInfo.filename());
             xhr.send(formData);
             },
-            file_picker_types: 'image',
-            file_picker_callback: function(cb, value, meta) {
-                var input = document.createElement('input');
-                input.setAttribute('type', 'file');
-                input.setAttribute('accept', 'image/*');
-                input.onchange = function() {
-                    var file = this.files[0];
-
-                    var reader = new FileReader();
-                    reader.readAsDataURL(file);
-                    reader.onload = function () {
-                        var id = 'blobid' + (new Date()).getTime();
-                        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-                        var base64 = reader.result.split(',')[1];
-                        var blobInfo = blobCache.create(id, file, base64);
-                        blobCache.add(blobInfo);
-                        cb(blobInfo.blobUri(), { title: file.name });
-                    };
-                };
-                input.click();
-            }
+            
         });
 </script>
